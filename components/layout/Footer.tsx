932283a3696f42eb26af9d78a066544155d59aaf
unknown <hoangnguyen164@gmail.com>
2026-01-09 18:25:49 +0700

    update footer website's name

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="bg-surface border-b border-border">
       <Container>
         <div className="py-6 text-sm flex justify-between">
-          <span>© 2026 Đông Phong Ecommerce</span>
+          <span>© 2026 HN369 Ecommerce</span>
           <span>About | Contact | Social</span>
         </div>
       </Container>
